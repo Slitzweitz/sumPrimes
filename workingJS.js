1836fdc79846a579ed2dfb218c75b0a3472d6916
Slitzweitz <colinmbarlow@gmail.com>
2015-08-05 16:40:37 -0700

    Create workingJS.js

---
+++ workingJS.js
@@ -0,0 +1,43 @@
+function sumPrimes(num) {
+
+  var primes=[];
+
+  function isPrime(x){
+
+    var prime=true;
+
+    for(var i=2;i<=Math.sqrt(x);i++){
+
+      if(x%i===0){
+
+        prime=false;
+
+      }
+
+    }
+
+    if(prime){
+
+      primes.push(x);
+
+    }
+
+  }
+
+  for(var j=3;j<=num;j++){
+
+    isPrime(j);
+
+  }
+
+  var total = 2;
+
+  for (var y in primes) {
+
+    total += primes[y];
+
+  }
+
+  return total;
+
+}
